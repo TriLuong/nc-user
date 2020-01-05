480a70f832325a61ba9d25705cec1f9469caac7d
Tri Luong <triluongdl@gmail.com>
2020-01-05 11:51:12 +0700

    Modify route for student.

--- route/route.go
+++ route/route.go
@@ -16,18 +16,18 @@ func Private(e *echo.Echo) {
 }
 
 func Staff(e *echo.Echo) {
-	g := e.Group("/api/student/v1/staff")
+	g := e.Group("/api/v1/student/staff")
 	g.GET("/student/:studentID", handler.GetStudentById)
-	g.DELETE("/student/id/:studentID", handler.DeleteStudentById)
-	g.PUT("/student/id/:studentID", handler.UpdateStudentByID)
+	g.DELETE("/student/:studentID", handler.DeleteStudentById)
+	g.PUT("/student/:studentID", handler.UpdateStudentByID)
 	g.POST("/student", handler.AddStudent)
 }
 
 func Public(e *echo.Echo) {
-	g := e.Group("/api/student/v1/public")
+	g := e.Group("/api/v1/student/public")
 	g.GET("/health", handler.HealthCheck)
 	g.GET("/test", handler.TestPublic)
 	g.GET("/student", handler.GetAllStudents)
-	g.GET("/student/id/:studentID", handler.GetStudentById)
+	g.GET("/student/:studentID", handler.GetStudentById)
 	g.GET("/student/simple", handler.SearchStudentSimple)
 }
